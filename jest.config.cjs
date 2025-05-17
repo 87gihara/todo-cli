1b94ce96b69cfe1caf8ef8e1ee3dfec28dfd5413
Kosuke Yanagihara <gihara.87k@gmail.com>
2025-05-17 14:58:34 +0900

    fix(jest): remove obsolete extensionsToTreatAsEsm

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.js'], // *.js を ESM 扱い
   transform: {},                   // 余計な Babel 変換をしない
 };
